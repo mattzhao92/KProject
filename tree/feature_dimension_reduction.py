e48695a42e18ebb338c6ad46f438d0e8d860f22c
xelliotpenguin <zhenghuan.zhao@gmail.com>
2014-03-22 16:12:04 -0500

    20 seconds per case

--- tree/feature_dimension_reduction.py
+++ tree/feature_dimension_reduction.py
@@ -3,15 +3,28 @@ from sklearn.decomposition import TruncatedSVD
 import numpy as np
 
 max_number_features = 2000000
+
+# print 'loading test data'
+# X_test, Y_test =   load_svmlight_file("test-remapped.csv", n_features=max_number_features, multilabel=True)
+
 print 'loading traininig data'
-X_train, Y_train = load_svmlight_file("train-fast.csv", n_features=max_number_features, multilabel=True)
-print 'loading test data'
-X_test, Y_test = load_svmlight_file("test-fast.csv", n_features=max_number_features, multilabel=True)
+X_train, Y_train = load_svmlight_file("train-quarter-remapped.csv", n_features=max_number_features, multilabel=True)
 
-svd = TruncatedSVD(n_components=200)
-X_test_reduced = svd.fit_transform(X_test)
-np.savetxt('X_test_reduced.txt', X_test_reduced)
 
-svd = TruncatedSVD(n_components=200)
-X_train_reduced = svd.fit_transform(X_train)
-np.savetxt('X_train_reduced.txt', X_train_reduced)
+# print 'begins test data reduction'
+# svd = TruncatedSVD(n_components=200)
+# X_test_reduced = svd.fit_transform(X_test)
+# np.savetxt('X_test_reduced.txt', X_test_reduced)
+
+# print 'begins training data reduction'
+# svd = TruncatedSVD(n_components=200)
+# X_train_reduced = svd.fit_transform(X_train)
+# np.savetxt('X_train_reduced.txt', X_train_reduced)
+
+# Generating training data labels
+# training_labels = open('train-labels', 'w')
+
+# for i in range(len(Y_train)):
+# 	y_row = [str(int(n)) for n in Y_train[i]]
+# 	training_labels.write(' '.join(y_row)+"\n")
+# training_labels.close()
--- tree/knn.py
+++ tree/knn.py
@@ -1,8 +1,9 @@
-import sys
+import sys, time
 import json
 import random
 import numpy as np
 import heapq, random
+from scipy import spatial
 from scipy.sparse import csr_matrix
 from sklearn.svm import LinearSVC
 from sklearn.datasets import load_svmlight_file
@@ -17,70 +18,40 @@ max_number_features = 2000000
 # Knn classifier benchmark
 
 print 'reading testing data ... '
-X_test,Y_test = load_svmlight_file("test-remapped.csv",n_features=max_number_features, multilabel=True)
+X_test= np.loadtxt("X_test_reduced.txt")
+
 
 print 'reading training data ... '
-X_train,Y_train = load_svmlight_file("train-remapped.csv",n_features=max_number_features, multilabel=True)
+X_train = np.loadtxt("X_train_reduced.txt")
+Y_train = []
+train_label_file = open("train-labels.txt", "rw+")
 
-print 'computing inverse_document_frequency from traninng data'
-transformer = TfidfTransformer()
-transformer.fit_transform(X_train)
-idf_weights = csr_matrix(transformer.idf_)
+for labels in train_label_file:
+	Y_train.append(labels.split())
 
-
-def calculateDistance(doc1, doc2, idf_weights):
-	assert doc1.shape == doc2.shape
-
-	doc2_tdidf = doc2.multiply(idf_weights)
-	doc2_nonzero_indices = doc2.nonzero()[1]
-	top3Features = heapq.nlargest(3, doc2_nonzero_indices, key=lambda x: doc2_tdidf[0,x])
-	
-	featureSet1 = set(doc1.nonzero()[1])
-	# check there is at least one feature in doc1 that appears in topThreeFeatures
-	topFeaturePresent = False
-	for topFeature in top3Features:
-		if topFeature in featureSet1:
-			topFeaturePresent = True
-
-	if not topFeaturePresent :
-		return 10000
-	
-	featureSet2 = set(doc2_nonzero_indices)
-	commonFeatureSet = featureSet1.intersection(featureSet2)
-
-	numFeature1 = len(featureSet1)
-	numFeature2 = len(featureSet2)
-	numCommonFeature = len(commonFeatureSet)
-
-	return 1.0 * (numFeature1 + numFeature2 - 2 * numCommonFeature) / (numFeature1 + numFeature2 - numCommonFeature)
+def calculateDistance(doc1, doc2):
+	return spatial.distance.cosine(doc1, doc2)
 
 print 'start classification ... '
 outputfile = open('output','w')
 outputfile.write("Id,Predicted\n")
 
-for i in range(X_test.shape[0]):
+for i in range(len(X_test)):
+
+	print '\r>> You have finished %d iter %d%%' % (i+1, (100.0 * i/len(X_test))),
+	sys.stdout.flush()
+
 	doc_distance_pairs = []
-	test_doc = X_test.getrow(i)
-	
+	test_doc = X_test[0]
 	
 	# for each train_doc , calculate its distance to test_doc
-	for j in range(X_train.shape[0]):
-		train_doc = X_train.getrow(j)
-		dist = calculateDistance(train_doc, test_doc, idf_weights)
-		if dist < 10000:
-			doc_distance_pairs.append((j, dist))
+	for j in range(len(X_train)):
+		train_doc = X_train[j]
+		dist = calculateDistance(train_doc, test_doc)
+		doc_distance_pairs.append((j, dist))
+
+
 	n_closest = heapq.nsmallest(5, doc_distance_pairs, key=lambda pair: pair[1])
-
-	new_n_closest = []
-	foundPerfect = False
-	for i in range(len(n_closest)):
-		if not foundPerfect:
-			if n_closest[i][1] == 0.0:
-				new_n_closest.append(n_closest[i])
-				foundPerfect = True
-				break
-
-	n_closest = new_n_closest
 
 	n_label_indices = [x[0] for x in n_closest]	
 	results = []
@@ -90,7 +61,6 @@ for i in range(X_test.shape[0]):
 		results.extend(labels)
 		if len(results) >= 3:
 			break
-	#print results
 
 	outputfile.write(str(i+1) +",")
 	if len(results) > 0:
@@ -98,6 +68,7 @@ for i in range(X_test.shape[0]):
 	else:
 		outputfile.write("0\n")
 outputfile.close()
+
 #print 'start preprocessing tranining data ... '
 
 # trans = TfidfTransformer()
