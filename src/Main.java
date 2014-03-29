6be457d3b1facd01b198adb8d0c8a0a928d8cb59
xelliotpenguin <zhenghuan.zhao@gmail.com>
2014-03-29 17:09:06 -0500

    removed src and tree folders

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-	public static void main(String [] args) {
-		String absPath = "/SETME";
-		NaiveBayesClassifier classifier = new NaiveBayesClassifier(absPath+"train.txt");
-		classifier.learn();
-		classifier.test(absPath+"test.txt");
-	}
-}
